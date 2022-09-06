761a87c952e382a5ab4b8ed4eb89eb2dff15964b
Krishna <krishnadevi.chauhan@outlook.com>
2022-09-07 00:38:09 +0530

    My First Commit

---
+++ src/main/loginPage.java
@@ -0,0 +1,10 @@
+public class loginPage {
+    public static void main(String[] args) {
+        System.out.print("Kroshna");
+        System.out.print("Kroshna");
+        System.out.print("Kroshna");
+        System.out.print("Kroshna");
+        System.out.print("Kroshna");
+        System.out.print("Kroshna");
+    }
+}
